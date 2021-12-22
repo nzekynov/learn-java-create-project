10a0b1a596f1806515e93ac4fd63f5f92d1f515c
Nikita Zekynov <nzekynov@inbox.ru>
2021-12-22 10:55:58 +0300

    Выпуск 5. Нестандартные операции.

--- src/SaveStudentOrder.java
+++ src/SaveStudentOrder.java
@@ -1,5 +1,9 @@
 public class SaveStudentOrder {
     public static void main(String[] args) {
-        System.out.println("SaveStudentOrder is running");
+        saveStudentOrder();
+    }
+
+    static void saveStudentOrder() {
+        System.out.println("saveStudentOrder");
     }
 }
