3746f30797d2fe0e3929b6b9934d443b32eb6aa1
Nikita Zekynov <nzekynov@inbox.ru>
2021-12-24 10:30:07 +0300

    Выпуск 12. Начинаем разбираться в наследовании.

--- src/edu/javacourse/studentorder/SaveStudentOrder.java
+++ src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -1,14 +1,11 @@
 package edu.javacourse.studentorder;
 
+import edu.javacourse.studentorder.domain.Adult;
 import edu.javacourse.studentorder.domain.StudentOrder;
 
 public class SaveStudentOrder {
     public static void main(String[] args) {
         StudentOrder so = new StudentOrder();
-        so.sethFirstName("Алексей");
-        so.sethLastName("Петров");
-        so.setwFirstName("Галина");
-        so.setwLastName("Петрова");
 
         long ans = saveStudentOrder(so);
         System.out.println(ans);
@@ -16,8 +13,17 @@ public class SaveStudentOrder {
 
     static long saveStudentOrder(StudentOrder studentOrder) {
         long answer = 199L;
-        System.out.println("saveStudentOrder: " + studentOrder.gethLastName());
+        System.out.println("saveStudentOrder");
 
         return answer;
+    }
+
+    static StudentOrder buildStudentOrder() {
+        StudentOrder so = new StudentOrder();
+        Adult husband = new Adult();
+        husband.setGivenName("Андрей");
+        so.setHusband(husband);
+
+        return so;
     }
 }
---
+++ src/edu/javacourse/studentorder/domain/Person.java
@@ -0,0 +1,51 @@
+package edu.javacourse.studentorder.domain;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String surname;
+    private String givenName;
+    private String patronymic;
+    private LocalDate dateOfBirth;
+    private Address address;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
